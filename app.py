37c3de2b9b31455f9fa8fea03052b4ce3eb3432e
Yousif Zuhair <yousifzuhairmail@gmail.com>
2023-06-22 00:10:33 +0100

    adding styling to app pt.1

--- app.py
+++ app.py
@@ -15,51 +15,85 @@ dates = list(data.Date)
 regions = list(data.Region)
 
 
+
 colors = {
-    'background': '#D3C0D2',
+    'background-dark': '#333533',
+    'background-light1': '#CFDBD5',
+    'background-light2': '#E8EDDF',
     'text': '#DAFFEF',
     'main-bg': '#D0FFD6',
     'title-tx': '#A6979C',
-    
+    'graph-bg': '#F5CB5C',
+    'graph-tx': '#242423'
 }
-
-
 
 df = pd.DataFrame({
     "Date": dates,
     "Sales": sales,
-    "Regions": regions
+    "Regions": regions,
 })
+
+reg = ['North', 'East', 'South', 'West']
 
 fig = px.line(df, x="Date", y="Sales", color="Regions")
 
 
 
 fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
+    plot_bgcolor=colors['graph-bg'],
+    paper_bgcolor=colors['graph-bg'],
+    font_color=colors['graph-tx']
 )
 
-app.layout = html.Div(style={'backgroundColor': colors['main-bg']}, children=[
-    html.H1(
-        children='Soul Foods Visualiser',
-        style={
-            'textAlign': 'left',
-            'color': colors['title-tx']
-        }
+
+
+
+
+# App layout and styling
+
+app.layout = html.Div([
+    html.Div(
+        className = "app-header",
+        children = [
+            html.Div('Soul Foods', className = "app-header--title")
+        ]
     ),
-
-    html.Div(children='Here is the visualization of Soul Food sales ordered through date.', style={
-        'textAlign': 'left',
-        'color': colors['title-tx']
-    }),
-
+    html.Div(
+        children = html.Div([
+            html.H3('Overview'),
+            html.P(''' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum pulvinar dui. Etiam vel est porta, viverra orci non, consectetur metus. Pellentesque ornare vitae urna vitae ultrices. Vivamus fermentum mi non turpis vulputate venenatis.
+                    Mauris suscipit suscipit vestibulum. Etiam sagittis dolor eros, at pulvinar nisl porttitor quis. Nullam consectetur lacinia purus eget tincidunt. Donec consequat nisl quis eros rhoncus eleifend. Nam sed risus ut ipsum condimentum laoreet ac ac neque. Donec sit amet porttitor urna, eu ullamcorper nulla. 
+                    Aenean consectetur magna ac nibh fermentum sagittis. Suspendisse egestas accumsan purus quis mollis. Quisque molestie venenatis orci quis sollicitudin.
+            ''')
+        ])
+    ),
     dcc.Graph(
-        id='example-graph-2',
-        figure=fig
-    )
+        id='line-graph-sales',
+        figure = fig,
+        className = "graph-line"
+    ),
+    dcc.RadioItems([
+        {'label': 'North', 'value': 'north'},
+        {'label': 'East', 'value': 'east'},
+        {'label': 'South', 'value': 'south'},
+        {'label': 'West', 'value': 'west'}
+    ], inline = True, id = 'region-radio')
+
 ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
+@app.callback(
+    Output('line-graph-sales','figure'),
+    Input('region-radio', 'value') 
+)
+def update_figure(selected_region):
+    filtered_df = df[df['Regions'] == selected_region]
+
+    fig = px.line(filtered_df, x = "Date", y = "Sales", colors = "Regions")
+
+    fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+
+    return fig
